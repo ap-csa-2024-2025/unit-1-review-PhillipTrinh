617ae5c605f590a1b93814ee554e010a9a8dc335
PhillipTrinh <pdtrinh100@student.hbuhsd.edu>
2024-09-13 11:20:56 -0700

    d

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -2,6 +2,16 @@ public class GradeCalculator
 {
   public static void main(String[] args)
   {
-    // insert code here
+    int hw1 = 44;
+    int hw2 =
+    int hw3 =
+    int hw4 =
+    
+    double quiz = 99.76;
+    double quiz2 = 82.99; 
+
+    double final = 79.66
+
+    DOUBLE avHw
   }
 }
